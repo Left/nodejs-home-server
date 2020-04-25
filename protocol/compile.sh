680bfd4b3665c9f5bf9d8d23b7343105adf3fc86
Left <vridosh@gmail.com>
2020-04-25 22:58:43 +0300

    Switcher

--- protocol/compile.sh
+++ protocol/compile.sh
@@ -1,3 +1,5 @@
+PATH=$PATH:~/equeumco/vcpkg/installed/x64-osx/tools/protobuf/
+
 protoc --nanopb_out=../../BluePillTest/src protocol.proto
 protoc --nanopb_out=../../MyWiFiController/src protocol.proto
 protoc --plugin=protoc-gen-ts=./../server/node_modules/.bin/protoc-gen-ts --js_out=import_style=commonjs,binary:./../server/generated --ts_out=service=grpc-web:./../server/generated protocol.proto
